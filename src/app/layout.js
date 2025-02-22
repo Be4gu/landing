39c5d254fc319170e2c74ce4ac51d70f9e86f955
Cristi <ioncristidaw@gmail.com>
2025-02-22 19:56:36 +0100

    Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <Script src='https://scripts.simpleanalyticscdn.com/latest.js' />
     </html>
   )
 }
