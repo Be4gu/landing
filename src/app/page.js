1d54db5007645637ceeb5230870aaf2e6590d61d
Cristi <ioncristidaw@gmail.com>
2025-02-25 20:44:53 +0100

    ON

--- src/app/page.js
+++ src/app/page.js
@@ -35,12 +35,12 @@ export default function Home() {
               >
                 <Twitch className='h-6 w-6 text-purple-500' />
                 <span className='text-xl font-semibold'>Entrellaves</span>
-                {/* <span class='absolute -right-1 -top-1'>
-                  <span class='relative flex h-3 w-3'>
-                    <span class='absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75'></span>
-                    <span class='relative inline-flex h-3 w-3 rounded-full bg-red-500'></span>
+                <span className='absolute -right-1 -top-1'>
+                  <span className='relative flex h-3 w-3'>
+                    <span className='absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75'></span>
+                    <span className='relative inline-flex h-3 w-3 rounded-full bg-red-500'></span>
                   </span>
-                </span> */}
+                </span>
               </a>
             </div>
           </div>
